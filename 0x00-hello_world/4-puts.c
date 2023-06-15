2937d8f956d448760e1191b197dbd1c4969942ee
Isaac <kingbygone@icloud.com>
2023-06-15 03:09:11 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-gcc puzzle.c -o puzzle
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints "\"Programming is like bulding a multilingua puzzle"
+ * Return: 0
+ */
+
+int main() {
+	puts("\n Programming is like building a multilingual puzzle");
+	return (0);
