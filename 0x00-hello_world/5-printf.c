0944598e2e0773b89501ffd0c69d589d8147bbd7
Isaac <kingbygone@icloud.com>
2023-06-15 04:08:29 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Printing a string
- * Description: use printf " with proper grammar, but the outcome is a piece of art,"
+ * Description: use printf
  * return: 0
  */
 int main(void)
