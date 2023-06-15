cc09800e9d54c5675fb7b4416c3bb7d0143da738
Isaac <kingbygone@icloud.com>
2023-06-15 06:26:24 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - principal file
+ * main - Entry point
+ *
  * return: 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is uself\"- Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
