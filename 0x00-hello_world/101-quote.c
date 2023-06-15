732f1b86611064d30927aeb139c79413285f04e2
Isaac <kingbygone@icloud.com>
2023-06-15 06:02:48 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - principal file
+ * return: 1
+ */
+int main(void)
+{
+	fprint(stderr, "and that piece of art is usuelf\n- Dora Korpar, 2015-10-19\n");
+		return (1);
+}
