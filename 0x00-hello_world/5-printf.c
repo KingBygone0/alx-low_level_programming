70791fdf4cd78b3d516ff7277911bbf7f99d14b0
Isaac <kingbygone@icloud.com>
2023-06-15 04:25:08 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - Print a string 
  * Description: use printf
- * return: 0
+ * Return: 0
  */
 
 int main(void)
 {
-	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
