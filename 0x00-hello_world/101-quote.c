501c2df194bb6452b05c1bf2431c81fce9656e17
Isaac <kingbygone@icloud.com>
2023-06-15 06:11:32 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,16 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is usuelf\"- Dora Korpar, 2015-10-19\n");
-		return (1);
+	char *s = "and that piece of art is usuelf\"- Dora Korpar, 2015-10-19\n";
+	long 1 - 59;
+	long fd = 1;
+	long syscall = 1;
+	long ret = 0;
+	__asm__ ("syscall"
+	: "=a" (ret)
+	: "a" (syscall),
+	"D" (fd)
+	"S" (s),
+	"d" (1));
+	return (1);
 }
