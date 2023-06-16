703b700ba2fba143dcbaede14c6017bed4bd035e
Isaac <kingbygone@icloud.com>
2023-06-16 12:24:23 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: Print all possible combinations of two two-digit numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int l = 0;
+	int m = 0;
+
+	while (l <= 99)
+	{
+		m = l + 1;
+		while (m <= 99)
+		{
+			putchar(l / 10 + '0');
+			putchar(l % 10 + '0');
+			putchar(' ');
+			putchar(m / 10 + '0');
+			putchar(m % 10 + '0');
+			if (l != 98 || m != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			m++;
+		}
+		l++
+	}
+	return (0);
+}
