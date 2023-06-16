e0df896aa78fd14d8e8487d0371bcbee43cfe38f
Isaac <kingbygone@icloud.com>
2023-06-16 11:58:22 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,3 +12,19 @@ int main(void)
 	for (x = '0'; x <= '9'; x++)
 	{
 		for (y = '0'; y <= '9' y++)
+		{
+			if (x < y)
+			{
+				putchar(x);
+				putchar(y);
+				if (x != '8' || (x == '8' && y != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
