0dd24ac34d5ebfca5cf7ec5eb4ceb8408ccc2d00
Isaac <kingbygone@icloud.com>
2023-06-16 08:21:54 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry Point
+ * Description: Print possible different combinations of three digits
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x, y;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = '0'; y <= '9' y++)
