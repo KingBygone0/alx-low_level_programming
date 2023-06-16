f47e7ff0f640c50364e9fc1330d856a92b8b716a
Isaac <kingbygone@icloud.com>
2023-06-16 05:29:13 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,6 +8,7 @@
 int main(void)
 {
 	char letter;
+	
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{putchar(letter);
 	}
@@ -15,5 +16,5 @@ int main(void)
 	{putchar(letter);
 	}
 	putchar ('\n');
-	return 0;
+	return (0);
 }
