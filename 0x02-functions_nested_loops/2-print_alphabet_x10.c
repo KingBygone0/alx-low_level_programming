9b7ca6b84c2319c048553d346d9f24d216eeb836
Isaac <kingbygone@icloud.com>
2023-06-20 06:08:34 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * main - check the code.
+ * print_alphabet_x10 - Entry point
  *
  * Return: Always 0 (success)
  */
 void print_alphabet_x10(void)
 {
 	int a, b;
+	
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 97; b <= 122; b++)
