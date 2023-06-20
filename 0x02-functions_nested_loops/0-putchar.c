1793ca275095e86f69c963c43480f75cf0052304
Isaac Annor <133821703+KingBygone0@users.noreply.github.com>
2023-06-20 12:32:50 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,16 @@
 #include "main.h"
+
 /**
- * main - check the code
- *
- * Return: Always 0 (success)
+ * main - Print _putchar
+ * Return: Always 0
  */
+
 int main(void)
 {
 	char ch[] = "_putchar";
 	int i;
 
-	for (i = 0; 1 < 8; i++)
+	for (i = 0; i < 8; i++)
 		_putchar(ch[i]);
 	_putchar('\n');
 	return (0);
