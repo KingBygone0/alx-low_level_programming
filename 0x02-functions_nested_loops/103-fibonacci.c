c091ee47a859f310265adc086bd8a5fdaa8654cf
Isaac <kingbygone@icloud.com>
2023-06-20 15:47:30 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -11,8 +11,11 @@ int main()
 int prev = 1;
 int curr = 2;
 int sum = 0;
-while (curr <= 4000000) {
-if (curr % 2 == 0) {
+
+while (curr <= 4000000)
+{
+if (curr % 2 == 0)
+{
 sum += curr;
 }
 int next = prev + curr;
