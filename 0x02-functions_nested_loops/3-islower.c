ef756d651995ec034e97e1c075616ec4e8442bc1
Isaac <kingbygone@icloud.com>
2023-06-20 04:29:03 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ * main - Check the code
+ *
+ * Return: Always 0 (success)
+ */
+{
+	int r;
+	r= _islower('H');
+	_putchar(r + '0');
+	r = _islower('0');
+	_putchar(r + '0');
+	r = _islower(108);
+	_[itcjar(r + '0');
+	_putchar('\n');
+	return(0);
+}
