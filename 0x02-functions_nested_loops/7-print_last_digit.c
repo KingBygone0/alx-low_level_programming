4d61fe9811db4eba27d8a2d268e033e4ce20081d
Isaac <kingbygone@icloud.com>
2023-06-20 07:28:42 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,11 @@
 int print_last_digit(int n)
 {
 	int last = n & 10;
+	
 	if (n < 0)
 	{
-	last = last * -n;
+	last = last * -1;
 	}
-	_putchar(last + '0')
+	_putchar(last + '0');
 	return (last);
 }
