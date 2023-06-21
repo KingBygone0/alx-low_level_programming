97f34a4a8a49828ac60812a05ff69afb7dc63970
Isaac Annor <133821703+KingBygone0@users.noreply.github.com>
2023-06-21 00:30:13 +0000

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,26 +7,43 @@
  */
 int main(void)
 {
-	int counter = 0;
-	unsigned long first = 0;
-	unsigned long second = 1;
-	unsigned long fib;
+	int count;
+	unsigned long x, y, z, a, b, c, carry;
 
-	while (counter <= 97)
+	x = 0;
+	y = 1;
+
+	for (count = 1; count <= 90; count++)
 	{
-		fib = first + second;
-		if (counter < 97)
-		{
-			printf("%lu, ", fib);
-		}
-		else
-		{
-			printf("%lu", fib);
-		}
-		first = second;
-		second = fib;
-		counter++;
+		z = x + y;
+		x = y;
+		y = z;
+		printf("%lu, ", z);
 	}
-	printf("\n");
+
+	a = x % 1000;
+	x = x / 1000;
+	b = y % 1000;
+	y = y / 1000;
+
+	while (count <= 98)
+	{
+		carry = (a + b) / 1000;
+		c = (a + b) - carry * 1000;
+		z = (x + y) + carry;
+		x = y;
+		y = z;
+		a = b;
+		b = c;
+
+		if (c >= 100)
+			printf("%lu%lu", z, c);
+		else
+			printf("%lu0%lu", z, c);
+		if (count < 98)
+			printf(", ");
+		count++;
+	}
+	putchar('\n');
 	return (0);
 }
