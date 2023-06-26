ecb5198d484998058d7776858b54381d3a8c62df
Isaac <kingbygone@icloud.com>
2023-06-26 06:34:08 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,30 +1,26 @@
 #include "main.h"
-#include <stdlib.h>
+
 /**
- * rev_string - prints
- * @s: The character to print
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- **/
+ * rev_string - prints a string in reverse
+ * @s: the used string to beb reversed
+ * Return: 0
+ */
+
 void rev_string(char *s)
 {
-int i;
-int large;
-char *cadena;
+	int len, i, half;
+	char temp;
 
-i = 0;
-while (s[i] != '\0')
-{
-i++;
-}
-large = i;
-cadena = (char *) malloc(large * sizeof(char));
-for (i = 0; i <= large; i++)
-{
-cadena[i] = (char) *(s + i);
-}
-for (i = 0; i <= large; i++)
-{
-*(s + i) = cadena[large - i - 1];
-}
+	for (len = 0; s[len] != '\0'; len++)
+	;
+	i = 0;
+	half = len / 2;
+
+	while (half--)
+	{
+		temp = s[len - i - 1];
+		s[len - i - 1] = s[i];
+		s[i] = temp;
+		i++;
+	}
 }
