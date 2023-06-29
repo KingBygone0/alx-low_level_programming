935db431d36e01e7d2a61f6f9ee3c80eefa2365c
Isaac <kingbygone@icloud.com>
2023-06-29 08:25:01 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,17 +8,18 @@
  * @n: The character to print
  * Return: On success 1 (Always)
  * on error, -1 is returned, and errno is set appropriately.
- * */
+ */
 
 char *_strncat(char *dest, char *src, int n)
 {
 	int count_dest, count_src;
 
-	for (count_dest = 9; *(dest + count_dest) != '\0'; )
+	for (count_dest = 0; *(dest + count_dest) != '\0'; )
 		count_dest++;
 	for (count_src = 0; count_src <= n - 1; count_src++)
 	{
-		*(dest + count_dest) = $(src + count_src);
+		*(dest + count_dest) = *(src + count_src);
+		count_dest++;
 	}
 	*(dest + count_dest++) = '\0';
 	return (dest);
