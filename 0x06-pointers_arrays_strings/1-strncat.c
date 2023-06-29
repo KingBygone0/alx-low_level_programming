14ec177a4d285bc6b890628c62aed34ab4b6b468
Isaac <kingbygone@icloud.com>
2023-06-29 08:27:03 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,21 +1,21 @@
-#include <main.h>
+#include "main.h"
 #include <string.h>
 
 /**
  * _strncat - prints
- * @dest: the character to print
+ * @dest: The character to print
  * @src: The character to print
  * @n: The character to print
- * Return: On success 1 (Always)
- * on error, -1 is returned, and errno is set appropriately.
- */
-
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ **/
 char *_strncat(char *dest, char *src, int n)
 {
 	int count_dest, count_src;
 
 	for (count_dest = 0; *(dest + count_dest) != '\0'; )
 		count_dest++;
+
 	for (count_src = 0; count_src <= n - 1; count_src++)
 	{
 		*(dest + count_dest) = *(src + count_src);
