61d136722ab31242b8cfb5e576ba83816a99ec52
Isaac <kingbygone@icloud.com>
2023-07-03 05:32:03 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+    char *p;
+    while (*s)
+    {
+        for (p = accept; *p; p++)
+        {
+            if (*s == *p)
+                return s;
+        }
+        s++;
+    }
+    return NULL;
+}
