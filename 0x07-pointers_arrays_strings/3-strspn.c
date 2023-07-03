3d6e56687fcfe7d081d6a5c1024c002646fa5fcb
Isaac <kingbygone@icloud.com>
2023-07-03 05:18:22 -0700

    Strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,22 +8,37 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int count = 0;
-	char *p;
-    
-	while (*s)
-    {
-        for (p = accept; *p; p++)
-        {
-            if (*s == *p)
-            {
-                count++;
-                break;
-            }
-        }
-        if (*p == '\0')
-            break;
-        s++;
-    }
-    return count;
+	int elements_accept;
+	int i, j;
+	unsigned int resul = 0;
+	int flag  = 0;
+
+	for (i = 0; *(accept + i) != '\0'; i++)
+		++elements_accept;
+	for (i = 0; *(s  + i) != '\0'; i++)
+	{
+		if (flag == 0)
+		{
+			flag = 0;
+			for (j = 0; *(accept + j) != '\0'; j++)
+			{
+				if (*(s + i) == *(accept + j))
+				{
+					resul = resul + 1;
+					flag = 0;
+					break;
+				}
+				else
+				{
+					flag = 1;
+				}
+
+			}
+		}
+		else
+		{
+			break;
+		}
+	}
+	return (resul);
 }
