9f9d48c07bb09163e204402e44f266193f241999
Isaac <kingbygone@icloud.com>
2023-07-05 03:43:46 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,40 +1,49 @@
-/**
+/*
  * File: 6-is_prime_number.c
  * Auth: Isaac Annor
 */
+
 #include "main.h"
 
-/**
- * _proceso - Return if the input is a prime number
- * @i: root to check of the number
- * @n: number
- * Return: 0 or 1
- */
 
-int _proceso(int i, int n)
+int is_divisible(int num, int div);
+int is_prime_number(int n);
+
+/**
+ * is_divisible - Checks if a number is divisible.
+ * @num: The number to be checked.
+ * @div: The divisor.
+ *
+ * Return: If the number is divisible - 0.
+ *         If the number is not divisible - 1.
+ */
+int is_divisible(int num, int div)
 {
-	if (n == 2)
-		return (2);
-	if (n % 2 == 0)
+	if (num % div == 0)
 		return (0);
-	if ((n % i == 0) && (n != i))
-		return (0);
-	if ((n % i == 0) && (n == i))
+
+	if (div == num / 2)
 		return (1);
-	return (_proceso(i + 1, n));
+
+	return (is_divisible(num, div + 1));
 }
 
-#include "main.h"
-
 /**
- * is_prime_number - Return if the input is a prime number
- * @n: Number
- * Return: 0 or 1
+ * is_prime_number - Checks if a number is prime.
+ * @n: The number to be checked.
+ *
+ * Return: If the integer is not prime - 0.
+ *         If the number is prime - 1.
  */
-
 int is_prime_number(int n)
 {
-	if (n < 2)
+	int div = 2;
+
+	if (n <= 1)
 		return (0);
-	return (_proceso(2, n));
+
+	if (n >= 2 && n <= 3)
+		return (1);
+
+	return (is_divisible(n, div));
 }
