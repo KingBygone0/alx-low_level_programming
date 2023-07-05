bb4ae913a8df64c8331ebd7461a75ec0391c9f9a
Isaac <kingbygone@icloud.com>
2023-07-05 02:55:49 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -13,12 +13,17 @@
  */
 int _strlen_recursion(char *s)
 {
-if (*s == '\0')
-{
-return 0;
-}
-else
-{
-return (1 + _strlen_recursion(s + 1));
-}
+	int length = 0;
+	int increment = 0;
+
+	if (*s != '\0')
+	{
+		increment++;
+		length += increment + _strlen_recursion(s + 1);
+		return (length);
+	}
+	else
+	{
+		return (0);
+	}
 }
