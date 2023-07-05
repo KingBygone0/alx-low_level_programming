d4d65f63f720594db0874972aa6ce66ae1e8b786
Isaac <kingbygone@icloud.com>
2023-07-05 03:23:35 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,39 +1,34 @@
+int _getsq(int, int);
 /**
- * File: 5-sqrt_recursion.c
- * Auth: Isaac Annor
- */
-#include "main.h"
-
-/**
- * _proceso - Return the square root of a number with iterators
- * @i: root to check of the number
- * @n: The number to find the square root of
- * Description: This function takes a number and returns its square root
- * Return: If i * i == n, return i. If i * i > n, returns -1.
- * Otherwise, returns the result of _proceso(i + 1, n).
- */
-int _proceso(int i, int n)
-{
-	if (i * i == n)
-		return (i);
-	else if (i * i > n)
-		return (-1);
-	else
-	return (_proceso(i + 1, n));
-}
-
-#include "main.h"
-/**
- * _sqrt_recursion - returns the natural square root of a number.
- * @n: number..
- * Description: This function takes a number and returns its square root
- * Return: numero.
- */
-
+ * _sqrt_recursion - root square
+ * @n: number to find the square root
+ * Return: -1 if a * a != n
+ **/
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	else
-	return (_proceso(0, n));
+	if (n == 0)
+		return (0);
+	if (n > 0)
+	{
+		return (_getsq(n, 1));
+	}
+	return (-1);
+}
+/**
+ *_getsq - do operations
+ *@n: number
+ *@a: base
+ *Return: -1 if a * a != n
+ **/
+int _getsq(int n, int a)
+{
+	if ((a * a) == n)
+		return (a);
+	if ((a * a) < n)
+		return (_getsq(n, (a + 1)));
+	if ((a * a) > n)
+		return (-1);
+	return (-1);
 }
