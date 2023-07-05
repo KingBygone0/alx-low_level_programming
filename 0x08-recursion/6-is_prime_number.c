25defaab0e98808675ab6901ce1d19b31bfe525a
Isaac <kingbygone@icloud.com>
2023-07-05 03:40:14 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,36 +5,44 @@
 #include "main.h"
 
 /**
- * _proceso - Return if the input is a prime number
- * @i: root to check of the number
- * @n: number
- * Return: 0 or 1
+ * finddiv - sqr
+ *
+ * @n: n
+ * @i: i
+ *
+ * Return: sqr root
+ *
  */
 
-int _proceso(int i, int n)
+int finddiv(int n, int i)
 {
-	if (n == 2)
-		return (2);
-	if (n % 2 == 0)
-		return (0);
-	if ((n % i == 0) && (n != i))
-		return (0);
-	if ((n % i == 0) && (n == i))
-		return (1);
-	return (_proceso(i + 1, n));
+	if (n % i == 0)
+		return (i);
+
+	else
+		return (finddiv(n, ++i));
+
+	return (0);
 }
 
-#include "main.h"
-
 /**
- * is_prime_number - Return if the input is a prime number
- * @n: Number
- * Return: 0 or 1
+ * is_prime_number - sqr
+ *
+ * @n: n
+ *
+ * Return: sqr root
  */
 
 int is_prime_number(int n)
 {
-	if (n < 2)
+	if (n <= 1)
 		return (0);
-	return (_proceso(2, n));
+
+	else if ((finddiv(n, 2)) == n)
+		return (1);
+
+	else
+		return (0);
+
+	return (0);
 }
