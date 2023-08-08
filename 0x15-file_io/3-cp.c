1c4b2bf4a8673a20f2afe3b815e6203b66d6d165
KingBygone0 <kingbygone@icloud.com>
2023-08-08 02:15:24 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,49 +1,41 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - copy a file into another file
- * @argc: amount of arguments
- * @argv: arguments
- * Return: a exit value if fails else return 0
- **/
-int main(int argc, char **argv)
+* main - Copy the contents of one file to another file.
+* @ac: Number of command-line arguments.
+* @av: Array of command-line argument strings.
+*
+* Return: (0) on Success (Always)
+*/
+int main(int ac, char **av)
 {
-	int fto = 0, fto_final = 0, ffrom = 0, rd1 = 0, wr2 = 0, cl1 = 0, cl2 = 0;
+	int fd_from, fd_to, read_from;
 	char buf[1024];
 
-	if (argc != 3)
+	if (ac != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]), exit(98);
+
+	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	while ((read_from = read(fd_from, buf, 1024)) > 0)
 	{
-		write(STDOUT_FILENO, "Usage: cp file_from file_to\n", 27);
-		exit(97);	}
-	ffrom = open(argv[1], O_RDONLY);
-	if (ffrom == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);	}
-	fto = open(argv[2], O_CREAT | O_EXCL);
-	if (fto < 0)
-		fto_final = open(argv[2], O_TRUNC);
-	if (fto >= 0)
-	{
-		close(fto);
-		fto_final = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);	}
-	for (rd1 = read(ffrom, buf, 1024); rd1 > 0; rd1 = read(ffrom, buf, 1024))
-	{
-		wr2 = write(fto_final, buf, rd1);
-		if (wr2 == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);		}
+		if (fd_to < 0 || (write(fd_to, buf, read_from) != read_from))
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
-	cl1 = close(fto_final);
-	if (cl1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %s", argv[2]);
-		exit(100);	}
-	cl2 = close(ffrom);
-	if (cl2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can´t close fd %s", argv[1]);
-		exit(100);	}
+
+	if (read_from == -1)
+		dprintf(STDERR_FILENO, "Error; Can't read from %s\n", av[1]), exit(98);
+
+	if ((close(fd_from)) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+
+	if ((close(fd_to)) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+
 	return (0);
 }
